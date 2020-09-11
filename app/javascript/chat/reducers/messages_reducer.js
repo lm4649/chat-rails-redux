c0ba5223c59c7c34541343f0df356edd6ab41326
marc lopes <marc_lopes2004@yahoo.fr>
2020-09-11 23:01:22 +0900

    fixed duplicated messages

--- app/javascript/chat/reducers/messages_reducer.js
+++ app/javascript/chat/reducers/messages_reducer.js
@@ -5,11 +5,14 @@ export default function(state = null, action) {
     case FETCH_MESSAGES: {
       return action.payload;
     }
-    case MESSAGE_POSTED: {
-      const copiedState = state.slice(0);
-      copiedState.push(action.payload);
-      return copiedState;
-    }
+    case 'MESSAGE_POSTED':
+     if (state.map(message => message.id).includes(action.payload.id)) {
+       return state;
+     } else {
+       const copiedState = state.slice(0);
+       copiedState.push(action.payload);
+       return copiedState;
+     }
     case CHANNEL_SELECTED: {
       return []; // Channel has changed. Clearing view.
     }
